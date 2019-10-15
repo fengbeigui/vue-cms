2819cb745dfded27b57527cbfca2f25a29f5aab2
fengbeigui <fbg0618@163.com>
2019-10-16 00:40:30 +0800

    完成首页轮播图样式布局

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -1,15 +1,38 @@
 <template>
   <div>
-      HomeContainer.vue
+    <!-- 轮播图区域 -->
+    <mt-swipe :auto="4000">
+      <!-- 在组件中，使用v-for循环的话，一定要使用 key -->
+      <mt-swipe-item >1</mt-swipe-item>
+      <mt-swipe-item >2</mt-swipe-item>
+      <mt-swipe-item >3</mt-swipe-item>
+    </mt-swipe>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped lang="scss">
+.mint-swipe {
+  height: 200px;
 
+  .mint-swaipe-item {
+    &:nth-child(1) {
+      background-color: red;
+    }
+    &:nth-child(2) {
+      background-color: blue;
+    }
+    &:nth-child(3) {
+      background-color: cyan;
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
 </style>
