a20977c9ed80898c05a586ca2f39223f21cf6586
fengbeigui <fbg0618@163.com>
2019-10-16 00:17:08 +0800

    完成tabbar路由链接的改造和路由高l亮

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 // 入口文件
 import Vue from 'vue'
 
+// 1.1 导入路由的包
+import VueRouter from 'vue-router'
+// 1.2 安装路由
+Vue.use(VueRouter)
+
+// 2.1 导入 vue-resource
+import VueResource from 'vue-resource'
+// 2.2 安装 vue-resource
+Vue.use(VueResource)
+
 //导入 MUI的样式
 import './lib/mui/css/mui.min.css';
 import './lib/mui/css/icons-extra.css'
@@ -10,10 +20,14 @@ import { Header } from 'mint-ui'
 Vue.component(Header.name, Header)
 
 
+// 1.3 导入自己的 router.js 路由模块
+import router from './router.js'
+
 // 导入 App 根组件
 import app from './App.vue'
 
 var vm = new Vue({
-  el: '#app',
-  render: c => c(app)
+    el: '#app',
+    render: c => c(app),
+    router // 1.4 挂载路由对象到 VM 实例上
 })
