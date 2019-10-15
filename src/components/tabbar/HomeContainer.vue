95addc74c34a75accb394b7e9e01072127eb4db4
fengbeigui <fbg0618@163.com>
2019-10-16 01:15:13 +0800

    完成首页中九宫格改造工程

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -4,23 +4,62 @@
     <mt-swipe :auto="4000">
       <!-- 在组件中，使用v-for循环的话，一定要使用 key -->
       <mt-swipe-item v-for="item in lunbotuList" :key="item">
-        <img :src="item.img" alt="">
+        <img :src="item.img" alt />
       </mt-swipe-item>
-
     </mt-swipe>
+
+    <!-- 九宫格 到 6宫格 的改造工程 -->
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu1.png" alt />
+          <div class="mui-media-body">新闻资讯</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu2.png" alt />
+          <div class="mui-media-body">图片分享</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu3.png" alt />
+          <div class="mui-media-body">商品购买</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu4.png" alt />
+          <div class="mui-media-body">留言反馈</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu5.png" alt />
+          <div class="mui-media-body">视频专区</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu6.png" alt />
+          <div class="mui-media-body">联系我们</div>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import {Toast} from 'mint-ui'
+import { Toast } from "mint-ui";
 
 export default {
   data() {
     return {
-      lunbotuList:[] //保存轮播图的数组
+      lunbotuList: [] //保存轮播图的数组
     };
   },
-  created(){
+  created() {
     this.getLunbotu();
   },
   methods: {
@@ -31,13 +70,13 @@ export default {
         .get("http://www.liulongbin.top:3005/api/getlunbo")
         .then(res => {
           //console.log(res.body);
-          if(res.body.status === 0){
+          if (res.body.status === 0) {
             //成功的话
             this.lunbotuList = res.body.message;
-            Toast('获取加载轮播图ok')
-          }else{
+            
+          } else {
             //失败的话
-              Toast('获取加载轮播图失败')
+            Toast("获取加载轮播图失败");
           }
         });
     }
@@ -65,5 +104,19 @@ export default {
       height: 100%;
     }
   }
+}
+.mui-grid-view.mui-grid-9 {
+  background-color: #fff;
+  border:none;
+  img{
+    width: 60px;
+    height: 60px;
+  }
+   .mui-media-body{
+     font-size: 13px;
+   }
+}
+.mui-grid-view.mui-grid-9 .mui-table-view-cell{
+  border:0;
 }
 </style>
