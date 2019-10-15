a0698420fdb64a067c92180e794a3596e72b9886
fengbeigui <fbg0618@163.com>
2019-10-16 00:56:29 +0800

    首页轮播图样式布局（获取到数据）

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -3,22 +3,52 @@
     <!-- 轮播图区域 -->
     <mt-swipe :auto="4000">
       <!-- 在组件中，使用v-for循环的话，一定要使用 key -->
-      <mt-swipe-item >1</mt-swipe-item>
-      <mt-swipe-item >2</mt-swipe-item>
-      <mt-swipe-item >3</mt-swipe-item>
+      <mt-swipe-item>1</mt-swipe-item>
+      <mt-swipe-item>2</mt-swipe-item>
+      <mt-swipe-item>3</mt-swipe-item>
     </mt-swipe>
   </div>
 </template>
 
 <script>
-export default {};
+import {Toast} from 'mint-ui'
+
+export default {
+  data() {
+    return {
+      lunbotuList:[] //保存轮播图的数组
+    };
+  },
+  created(){
+    this.getLunbotu();
+  },
+  methods: {
+    //获取轮播图数据的方法
+    getLunbotu() {
+      //看api文档
+      this.$http
+        .get("http://www.liulongbin.top:3005/api/getlunbo")
+        .then(res => {
+          //console.log(res.body);
+          if(res.body.status === 0){
+            //成功的话
+            this.lunbotuList = res.body.message;
+            Toast('获取加载轮播图ok')
+          }else{
+            //失败的话
+              Toast('获取加载轮播图失败')
+          }
+        });
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
 .mint-swipe {
   height: 200px;
 
-  .mint-swaipe-item {
+  .mint-swipe-item {
     &:nth-child(1) {
       background-color: red;
     }
