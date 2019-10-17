429703822bafcab3daaa825a26efa209b3e0d9ef
fengbeigui <fbg0618@163.com>
2019-10-18 00:12:05 +0800

    完成发表评论的功能

--- src/components/subcomponents/comment.vue
+++ src/components/subcomponents/comment.vue
@@ -2,10 +2,10 @@
   <div>
     <h3>发表评论</h3>
     <hr />
-    <textarea placeholder="请输入要BB的内容（做多吐槽120字）" maxlength="120"></textarea>
+    <textarea placeholder="请输入要BB的内容（做多吐槽120字）" maxlength="120" v-model="msg"></textarea>
 
     <!-- mt-button按钮是在main中添加的 -->
-    <mt-button type="primary" size="large">发表评论</mt-button>
+    <mt-button type="primary" size="large" @click="postComment">发表评论</mt-button>
 
     <div class="cmt-list">
       <div class="cmt-item" v-for="(item, i ) in comments" :key="i">
@@ -27,7 +27,8 @@ export default {
   data() {
     return {
       pageIndex: 1, //默认展示第一页数据
-      comments: [] //定义所有的评论为空数据
+      comments: [], //定义所有的评论为空数据
+      msg: "" //评论输入的内容
     };
   },
   //调用
@@ -53,6 +54,33 @@ export default {
     getMore() {
       this.pageIndex++;
       this.getComments();
+    },
+    //发表评论
+    postComment() {
+      //校验是否为空内容
+      if (this.msg.trim().length === 0) {
+        return Toast("评论内容不能为空");
+      }
+      //参数1  请求的URL地址
+      //参数2 提交服务器的数据对象 {content:this.msg}
+      //参数3 定义提交时候，表单中数据的格式
+      //content不是随便定义的 接口文档提供的
+      this.$http
+        .post("api/postcomment/" + this.$route.params.id, {
+          content: this.msg.trim()
+        })
+        .then(function(res) {
+          if (res.body.status === 0) {
+            //拼接出一个评论对象
+            var cmt = {
+              user_name: "匿名用户",
+              add_time: Date.now(),
+              content: this.msg.trim()
+            };
+          }
+          this.comments.unshift(cmt);
+          this.msg = "";
+        });
     }
   },
   //子组件想用这个id的话，需要props定义
--- src/main.js
+++ src/main.js
@@ -21,6 +21,8 @@ Vue.use(VueResource)
 
 // 设置请求的根路径
 Vue.http.options.root = 'http://www.liulongbin.top:3005';
+//全局设置post 时候表单数据格式组织形式
+Vue.http.options.emulateJSON = true;
 
 //导入 MUI的样式
 import './lib/mui/css/mui.min.css';
