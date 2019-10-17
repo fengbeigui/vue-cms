3862fdf9aaed4253f3c296262a810ce58b2afa6f
fengbeigui <fbg0618@163.com>
2019-10-18 02:21:39 +0800

    获取对应分类的图片列表数据并渲

--- src/components/photos/PhotoList.vue
+++ src/components/photos/PhotoList.vue
@@ -11,10 +11,22 @@
             :class="['mui-control-item ',item.id == 0 ? 'mui-active' : '']"
             v-for="(item,index) in cates"
             :key="index"
+            @tap="getPhotoListByCateId(item.id)"
           >{{item.title}}</a>
         </div>
       </div>
     </div>
+
+    <!-- 图片列表区域 -->
+    <ul class="photo-list">
+      <router-link v-for="item in list" :key="item.id" :to="'/home/photoinfo/' + item.id" tag="li">
+        <img v-lazy="item.img_url" />
+        <div class="info">
+          <h1 class="info-title">{{ item.title }}</h1>
+          <div class="info-body">{{ item.zhaiyao }}</div>
+        </div>
+      </router-link>
+    </ul>
   </div>
 </template>
 
@@ -25,12 +37,15 @@ import mui from "../../lib/mui/js/mui.min.js";
 export default {
   data() {
     return {
-      cates: [] //所有分类的列表数组
+      cates: [], //所有分类的列表数组
+      list: [] // 图片列表的数组
     };
   },
   //方法调用
   created() {
     this.getAllCategory();
+    // 默认进入页面，就主动请求 所有图片列表的数据
+    this.getPhotoListByCateId(0);
   },
   mounted() {
     // 当 组件中的DOM结构被渲染好并放到页面中后，会执行这个 钩子函数
@@ -49,6 +64,14 @@ export default {
           this.cates = res.body.message;
         }
       });
+    },
+    // 根据 分类Id，获取图片列表
+    getPhotoListByCateId(cateId) {
+      this.$http.get("api/getimages/" + cateId).then(result => {
+        if (result.body.status === 0) {
+          this.list = result.body.message;
+        }
+      });
     }
   }
 };
@@ -59,5 +82,42 @@ export default {
 //使用全局样式样式去掉
 * {
   touch-action: pan-y;
+}
+.photo-list {
+  list-style: none;
+  margin: 0;
+  padding: 10px;
+  padding-bottom: 0;
+  li {
+    background-color: #ccc;
+    text-align: center;
+    margin-bottom: 10px;
+    box-shadow: 0 0 9px #999;
+    position: relative;
+    img {
+      width: 100%;
+      vertical-align: middle;
+    }
+    img[lazy="loading"] {
+      width: 40px;
+      height: 300px;
+      margin: auto;
+    }
+
+    .info {
+      color: white;
+      text-align: left;
+      position: absolute;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.4);
+      max-height: 84px;
+      .info-title {
+        font-size: 14px;
+      }
+      .info-body {
+        font-size: 13px;
+      }
+    }
+  }
 }
 </style>
