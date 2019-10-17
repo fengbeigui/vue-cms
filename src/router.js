16f5bd9fe1b5b02287152270b3522e989f89afa0
fengbeigui <fbg0618@163.com>
2019-10-18 00:22:43 +0800

    改造图片分析按钮为路由的链接

--- src/router.js
+++ src/router.js
@@ -9,6 +9,8 @@ import SearchContainer from './components/tabbar/SearchContainer.vue'
 import NewsList from './components/news/NewsList.vue'
 //新闻详情
 import NewsInfo from './components/news/NewsInfo.vue'
+//图片
+import PhotoList from './components/photos/PhotoList.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
@@ -19,7 +21,8 @@ var router = new VueRouter({
     { path: '/shopcar', component: ShopcarContainer },
     { path: '/search', component: SearchContainer },
     { path: '/home/newslist', component: NewsList },
-    { path: '/home/newsinfo/:id', component: NewsInfo }
+    { path: '/home/newsinfo/:id', component: NewsInfo },
+    { path: '/home/photolist', component: PhotoList }
   ],
   linkActiveClass: 'mui-active' // 覆盖默认的路由高亮的类，默认的类叫做 router-link-active
 })
