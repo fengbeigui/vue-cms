626392940398d369e12dd901d837fc96280f4ce9
fengbeigui <fbg0618@163.com>
2019-10-17 19:24:39 +0800

    绘制新闻资讯列表页面布局

--- src/components/news/NewsList.vue
+++ src/components/news/NewsList.vue
@@ -1,15 +1,59 @@
 <template>
   <div>
-      新闻列表
+    <ul class="mui-table-view">
+      <li class="mui-table-view-cell mui-media">
+        <a href="javascript:;">
+          <img class="mui-media-object mui-pull-left" src="/images/menu1.png" />
+          <div class="mui-media-body">
+            <h1>幸福</h1>
+            <p class="mui-ellipsis">能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办？</p>
+            <span>发表时间:2019-10-10 10:10:10</span>
+            <span>点击:0次</span>
+          </div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media">
+        <a href="javascript:;">
+          <img class="mui-media-object mui-pull-left" src="/images/menu1.png" />
+          <div class="mui-media-body">
+            <h1>幸福</h1>
+            <p class="mui-ellipsis">能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办？</p>
+            <span>发表时间:2019-10-10 10:10:10</span>
+            <span>点击:0次</span>
+          </div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media">
+        <a href="javascript:;">
+          <img class="mui-media-object mui-pull-left" src="/images/menu1.png" />
+          <div class="mui-media-body">
+            <h1>幸福</h1>
+            <p class="mui-ellipsis">能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办？</p>
+            <span>发表时间:2019-10-10 10:10:10</span>
+            <span>点击:0次</span>
+          </div>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.mui-table-view{
+    li{
+        h1{
+            font-size: 14px;
+        }
+        .mui-ellipsis{
+            font-size: 12px;
+            color: #226aff;
+            display:flex;
+            justify-content: space-between;
+        }
+    }
+}
 </style>
