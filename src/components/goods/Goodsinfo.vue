0b43c6d65a487a8a70c2f510593c13c5ace39d0e
fengbeigui <fbg0618@163.com>
2019-10-20 00:18:18 +0800

    绘制商品详情页面的卡片视图

--- src/components/goods/Goodsinfo.vue
+++ src/components/goods/Goodsinfo.vue
@@ -1,15 +1,39 @@
 <template>
-  <div>
-      商品详细页面
+  <div class="goods-info-item">
+    <!--   商品轮播图区域 -->
+    <div class="mui-card">
+      <div class="mui-card-content">
+        <div class="mui-card-content-inner">这是一个最简单的卡片视图控件；卡片视图常用来显示完整独立的一段信息，比如一篇文章的预览图、作者信息、点赞数量等</div>
+      </div>
+    </div>
+
+    <!-- 商品购买区 -->
+    <div class="mui-card">
+      <div class="mui-card-header">页眉</div>
+      <div class="mui-card-content">
+        <div class="mui-card-content-inner">包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）</div>
+      </div>
+    </div>
+
+    <!-- 商品参数区 -->
+    <div class="mui-card">
+      <div class="mui-card-header">页眉</div>
+      <div class="mui-card-content">
+        <div class="mui-card-content-inner">包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）</div>
+      </div>
+      <div class="mui-card-footer">页脚</div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.goods-info-item{
+    background-color: #eee;
+    //解决头部白色塌陷部分
+    overflow: hidden;
+}
 </style>
