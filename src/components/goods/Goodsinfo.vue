4d01cb4939e5d01a3602bf1d150507dac2a40343
fengbeigui <fbg0618@163.com>
2019-10-20 18:48:58 +0800

    完美实现小球动画适配不同滚动

--- src/components/goods/Goodsinfo.vue
+++ src/components/goods/Goodsinfo.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="goods-info-item">
-
     <!-- 来个小球 -->
-   <!--  然后来个位移，使用transition -->
-   <!-- 第二部使用钩子函数 -->
-    <transition @before-enter="beforeEnter"
-    @enter="enter"
-    @after-enter="afterEnter"
-    >
-      <div class="ball" v-show="ballFlag"></div>
+    <!--  然后来个位移，使用transition -->
+    <!-- 第二部使用钩子函数 -->
+    <!-- 由于上面的小球在这个组件内，我们可以通过ref来获取到 -->
+    <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
+      <div class="ball" v-show="ballFlag" ref="ball"></div>
     </transition>
 
     <!--   商品轮播图区域 -->
@@ -34,7 +31,6 @@
           <div>
             <span>购买数量:</span>
             <div class="mui-numbox" data-numbox-min="1" data-numbox-max="9">
-             
               <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
               <input id="test" class="mui-input-numbox" type="number" value="5" />
               <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
@@ -79,10 +75,9 @@ export default {
       //轮播图的数据
       lunbotu: [],
       //声明一个默认给他空对象,表示获取到的商品的信息
-      goodsinfo:{},
+      goodsinfo: {},
       //控制小球隐藏和显示的标识符
-      ballFlag:false,
-      
+      ballFlag: false
     };
   },
   //调用事件，和mounted()相似
@@ -92,10 +87,7 @@ export default {
   },
   //注册一下
   components: {
-    swiper,
-   
-   
-    
+    swiper
   },
   methods: {
     getLunbotu() {
@@ -110,43 +102,64 @@ export default {
       });
     },
     //获取商品的信息
-    getGoodsInfo(){
-      this.$http.get('api/goods/getinfo/'+this.id).then(res=>{
-        if(res.body.status ===0){
+    getGoodsInfo() {
+      this.$http.get("api/goods/getinfo/" + this.id).then(res => {
+        if (res.body.status === 0) {
           this.goodsinfo = res.body.message[0];
         }
-      })
+      });
     },
     //点击使用编程式导航跳转到图文介绍页面
-    goDesc(id){
+    goDesc(id) {
       //[Vue warn]: Error in v-on handler: "ReferenceError: id is not defined" found in  说明没有传参数id
-      this.$router.push({name:"goodsdesc",params:{id}})
+      this.$router.push({ name: "goodsdesc", params: { id } });
     },
     //点击跳转到评论页面
-    goComment(id){
-       this.$router.push({name:"goodscomment",params:{id}})
+    goComment(id) {
+      this.$router.push({ name: "goodscomment", params: { id } });
     },
     //点击添加到购物车
-    addToShopCar(){
+    addToShopCar() {
       //取反，点击显示,再次点击切换隐藏
-      this.ballFlag = !this.ballFlag
-    },
-    //三个钩子函数定义一下 beforeEnter  enter afterEnter
-    beforeEnter(el){
-      //小球样式 0 0 点是数字那个位置
-        el.style.transform = "translate(0,0)";
-    },
-    enter(el,done){
-      el.offsetWidth;
-      //小球位移的终点
-      el.style.transform = 'translate(93px,230px)';
-      //过渡的动效
-      //el.style.transition = 'all 1s ease'
-      el.style.transition = 'all 1s cubic-bezier(.4,-0.3,1,.68)';
-    },
-    afterEnter(el){
       this.ballFlag = !this.ballFlag;
     },
+    //三个钩子函数定义一下 beforeEnter  enter afterEnter
+    //小球动画优化思路
+    //1 先分析导致 动画 不准确的 本质原因：我们把小球位移的位置 已经局限在了某一分辨率下的   滚动条未滚动的情况下
+    //2 只要分辨率和测试的时候不一样 或者 滚动条有一定的滚动距离后，问题就出现了
+    //3 因此  我们经过分析，得到结论：不能把 位置的 横纵坐标 直接写死了  而是应该 根据不同的情况  动态计算这个坐标值
+    //4 经过分析 得出解题思路  先得到 徽标的 横纵坐标，再得到 小球的横纵坐标，然后 让y值 求差，x 值也求差，得到的结果，就是横纵坐标要位移的距离
+    //5  如何 获取 徽标和小球的位置？？？  domObject.getBoundingClientRect()
+    beforeEnter(el) {
+      //小球样式 0 0 点是数字那个位置
+      el.style.transform = "translate(0,0)";
+    },
+    enter(el, done) {
+      el.offsetWidth;
+
+      //获取小球在页面中的位置,上面定义了ref,这里可以通过this.$refs拿到小球
+      const ballPosition = this.$refs.ball.getBoundingClientRect();
+      //购物车的徽标，vue不推荐我们操作dom,不涉及数据，我们简单用下dom也是可以的
+      //在App.vue的徽标哪里添加id="badge"；这边就能获取到了元素;再实现获取徽标在页面中的位置
+      const badgePosition = document
+        .getElementById("badge")
+        .getBoundingClientRect();
+
+      //实现拿到位置
+      const xDist = badgePosition.left - ballPosition.left;
+      const yDist = badgePosition.top - ballPosition.top;
+
+      //小球位移的终点,es6拼接字符串
+      el.style.transform = `translate(${xDist}px,${yDist}px)`;
+      //过渡的动效
+      //el.style.transition = 'all 1s ease'
+      el.style.transition = "all 1s cubic-bezier(.4,-0.3,1,.68)";
+      //需要点击done
+      done();
+    },
+    afterEnter(el) {
+      this.ballFlag = !this.ballFlag;
+    }
   }
 };
 </script>
@@ -163,17 +176,17 @@ export default {
   //数字值 400 相当于 关键字 normal，700 等价于 bold
   font-weight: bold;
 }
-.mui-card-footer{
+.mui-card-footer {
   //把父元素的flex去掉，就可以上下
   display: block;
 
-  button{
+  button {
     margin: 15px 0;
   }
 }
 
 //小球样式
-.ball{
+.ball {
   //初始
   width: 15px;
   height: 15px;
@@ -186,7 +199,5 @@ export default {
   left: 146px;
   //层级
   z-index: 99;
-
-  
 }
 </style>
