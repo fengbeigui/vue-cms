b1617b327869f26d454ff34749832f9a6fcff92d
fengbeigui <fbg0618@163.com>
2019-10-20 17:22:43 +0800

    实现商品详情中的图文介绍和评论

--- src/components/goods/GoodsComment.vue
+++ src/components/goods/GoodsComment.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-      <h3>商品评论页面</h3>
+    <!-- 需要路由里面的那个id传过来 -->
+   <!--  query要用path来引入，params要用name来引入，接收参数都是类似的，分别是this.$route.query.name和this.$route.params.name -->
+   <!-- query更加类似于我们ajax中get传参，params则类似于post -->
+    <cmtbox :id="$route.params.id"></cmtbox>
   </div>
 </template>
 
 <script>
+//导入组件
+import cmtbox from "../subcomponents/comment.vue";
 export default {
-
-}
+  data() {
+    return {};
+  },
+  //注册组件
+  components: {
+    cmtbox
+  }
+};
 </script>
 
 <style>
-
 </style>
--- src/components/goods/GoodsDesc.vue
+++ src/components/goods/GoodsDesc.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>
-      <h3>商品图文介绍</h3>
+  <div class="goodssdesc-container">
+    <h3>{{info.title}}</h3>
+    <hr>
+    <!--  内容带标签，使用v-html -->
+    <div class="content" v-html="info.content"></div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      info: {} //声明一个图文数据空对象
+    };
+  },
+  created() {
+    this.getGoodsDesc();
+  },
+  methods: {
+    getGoodsDesc() {
+      this.$http.get("api/goods/getdesc/" + this.$route.params.id).then(res => {
+        if (res.body.status === 0) {
+          this.info = res.body.message[0];
+        }
+      });
+    }
+  }
+};
 </script>
 
-<style>
+<style  lang="scss">
+.goodssdesc-container{
+    padding: 4px;
+    h3{
+        font-size: 16px;
+        color:#226aff;
+        text-align: center;
+        margin: 15px 0;
 
+    }
+    .content{
+        img{
+            width: 100%;
+        }
+    }
+
+}
 </style>
