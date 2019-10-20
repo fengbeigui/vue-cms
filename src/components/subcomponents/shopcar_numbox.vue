d53b671302465be286eb1a201b2e75e50eae73eb
fengbeigui <fbg0618@163.com>
2019-10-20 23:16:25 +0800

    绘制购物车页面中商品列表的布局

---
+++ src/components/subcomponents/shopcar_numbox.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <div class="mui-numbox" data-numbox-min="1" style="height:25px">
+    <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
+    <input
+      id="test"
+      class="mui-input-numbox"
+      type="number"
+      value="1"
+      @change="countChanged"
+      ref="numbox"
+    />
+    <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
+  </div>
+
+  <!-- 分析： 子组件什么时候把 数据传递给父组件 -->
+  <!--  -->
+</template>
+ 	
+ 	<script>
+import mui from "../../lib/mui/js/mui.min.js";
+
+export default {
+  mounted() {
+    // 初始化数字选择框组件
+    mui(".mui-numbox").numbox();
+
+  },
+  methods: {
+    countChanged() {
+        //数量改变了
+    }
+  },
+
+};
+</script>
+ 	
+ 	<style lang="scss" scoped>
+</style>
--- src/components/tabbar/ShopcarContainer.vue
+++ src/components/tabbar/ShopcarContainer.vue
@@ -1,15 +1,77 @@
 <template>
-  <div>
-      ShopcarContainer.vue
+  <div class="shopcar-container">
+    <div class="goods-list">
+      <!-- 商品列表项区域 -->
+      <div class="mui-card">
+        <div class="mui-card-content">
+          <div class="mui-card-content-inner">
+            <mt-switch ></mt-switch>
+            <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/3c1af9783bdc53ed843af5655ca92009.jpg?thumb=1&w=200&h=200&f=webp&q=90" >
+            <div class="info">
+              <h1>小米（mi）小米none 16G双网通版本</h1>
+              <p>
+                <span class="price">￥2199</span>
+                <shopcarNunbox></shopcarNunbox>
+                <a href="#">删除</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- 结算区域 -->
+      <div class="mui-card">
+        <div class="mui-card-content">
+          <div class="mui-card-content-inner">
+            这是一个最简单的卡片视图控件；卡片视图常用来显示完整独立的一段信息，比如一篇文章的预览图、作者信息、点赞数量等
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import shopcarNunbox from "../subcomponents/shopcar_numbox.vue";
 
-}
+export default {
+  data(){
+    return{
+
+    }
+  },
+  components:{
+      shopcarNunbox
+  },
+
+};
 </script>
 
-<style>
+<style scoped lang="scss">
+.shopcar-container{
+  background-color: #eee;
+  overflow: hidden;
 
+  .goods-list{
+    .mui-card-content-inner{
+      display: flex;
+      //居中
+      align-items: center;
+    }
+    img{
+      width: 60px;
+      height: 60px;
+    }
+    h1{
+      font-size: 13px;
+    }
+    .info{
+      .price{
+        color: red;
+        font-weight: 700;
+      }
+    }
+  }
+}
 </style>
