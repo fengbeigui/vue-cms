67f1317f2d1912703e9a5e69ad0c712777c075a2
fengbeigui <fbg0618@163.com>
2019-10-21 00:19:10 +0800

    获取购物车中所有商品列表并加载显示

--- src/components/tabbar/ShopcarContainer.vue
+++ src/components/tabbar/ShopcarContainer.vue
@@ -2,15 +2,17 @@
   <div class="shopcar-container">
     <div class="goods-list">
       <!-- 商品列表项区域 -->
-      <div class="mui-card">
+      <div class="mui-card" v-for="item in goodslist" :key="item.id">
         <div class="mui-card-content">
           <div class="mui-card-content-inner">
-            <mt-switch ></mt-switch>
-            <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/3c1af9783bdc53ed843af5655ca92009.jpg?thumb=1&w=200&h=200&f=webp&q=90" >
+            <mt-switch></mt-switch>
+            <img
+              :src="item.thumb_path"
+            />
             <div class="info">
-              <h1>小米（mi）小米none 16G双网通版本</h1>
+              <h1>{{item.title}}</h1>
               <p>
-                <span class="price">￥2199</span>
+                <span class="price">￥{{item.sell_price}}</span>
                 <shopcarNunbox></shopcarNunbox>
                 <a href="#">删除</a>
               </p>
@@ -22,12 +24,11 @@
       <!-- 结算区域 -->
       <div class="mui-card">
         <div class="mui-card-content">
-          <div class="mui-card-content-inner">
-            这是一个最简单的卡片视图控件；卡片视图常用来显示完整独立的一段信息，比如一篇文章的预览图、作者信息、点赞数量等
-          </div>
+          <div
+            class="mui-card-content-inner"
+          >这是一个最简单的卡片视图控件；卡片视图常用来显示完整独立的一段信息，比如一篇文章的预览图、作者信息、点赞数量等</div>
         </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -36,38 +37,60 @@
 import shopcarNunbox from "../subcomponents/shopcar_numbox.vue";
 
 export default {
-  data(){
-    return{
+  data() {
+    return {
+      goodslist:[] //购物车中所有商品的数据
+    };
+  },
+  components: {
+    shopcarNunbox
+  },
+  created(){
+    this.getGoodsList();
+  },
+  methods: {
+    //获取购物车商品列表
+    getGoodsList() {
+      //1.获取到store 中所有的商品的ID,然后拼接出一个 用逗号分隔的字符串
+      var idArr = [];
 
+      this.$store.state.car.forEach(item => idArr.push(item.id));
+      //如果购物车中没有商品，则直接返回，不需要请求数据接口，否则会报错
+      if(idArr.length<=0){
+        return
+      }
+
+      this.$http.get("api/goods/getshopcarlist/" + idArr.join(","))
+      .then(res=>{
+            if(res.body.status ===0){
+              this.goodslist = res.body.message;
+            }
+      });
     }
-  },
-  components:{
-      shopcarNunbox
-  },
-
+  }
 };
 </script>
 
 <style scoped lang="scss">
-.shopcar-container{
+.shopcar-container {
   background-color: #eee;
   overflow: hidden;
 
-  .goods-list{
-    .mui-card-content-inner{
+  .goods-list {
+    .mui-card-content-inner {
       display: flex;
       //居中
       align-items: center;
     }
-    img{
+    img {
       width: 60px;
       height: 60px;
     }
-    h1{
+    h1 {
       font-size: 13px;
     }
-    .info{
-      .price{
+    .info {
+      .price {
         color: red;
         font-weight: 700;
       }
